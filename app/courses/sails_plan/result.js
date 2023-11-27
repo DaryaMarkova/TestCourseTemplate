48caf7ccd39d53624e54efc70bc3728226613586
DaryaMarkova <darya.towarddreams@yandex.ru>
2023-11-27 06:53:22 +0300

    Работа над результирующим блоком вопросов

---
+++ app/courses/sails_plan/result.js
@@ -0,0 +1,40 @@
+(function() {
+  let questionCount = 0;
+
+  const questions$ = $('.next');
+  let currentQuestion$ = questions$.eq(questionCount);
+  
+  $('.multiquestion-item-answer').hide();
+
+  // доработать скрипт с реакцией на чекбоксы
+
+  function init() {
+    questions$.slice(1).hide();
+
+    $('#to_questions').click(function() {
+      const this$ = $(this);
+
+      this$.fadeOut();
+      this$.parent().find('.tooltip').fadeOut();
+      this$.parent().find('.text').fadeOut();
+
+      switchNextQuestion();
+    });
+
+    $('.show-answer').click(function() {
+      currentQuestion$.find('.multiquestion-item-answer').slideDown(400);
+    });
+
+    $('.next-question').click(function() {
+      currentQuestion$.hide(); 
+      switchNextQuestion();
+    })
+  }
+
+  function switchNextQuestion() {
+    questions$.eq(++questionCount).fadeIn(500);
+    currentQuestion$ = questions$.eq(questionCount);
+  }
+
+  init();
+})();
